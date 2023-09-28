353aec1a03424ecf870115d57d35c060a4edcacc
Guillermo C.C <guilleccubillos@hotmail.com>
2023-09-27 20:27:14 -0500

    Initial page with background

---
+++ lib/src/screens/initial_1/indesx.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          SvgPicture.asset(
+            'assets/images/sc_1/background1.svg',
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
