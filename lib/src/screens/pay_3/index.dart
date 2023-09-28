34189d3b7a0b19144bcf6dded6568567c8a1fa9d
Guillermo C.C <guilleccubillos@hotmail.com>
2023-09-28 00:05:21 -0500

    Finish screen 3

--- lib/src/screens/pay_3/index.dart
+++ lib/src/screens/pay_3/index.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../widgets/assets_fadeIn/index.dart';
+
 class PayScreen extends StatefulWidget {
   const PayScreen({super.key});
   @override
@@ -21,6 +23,87 @@ class _PayScreenState extends State<PayScreen> {
             height: MediaQuery.of(context).size.height,
             fit: BoxFit.cover,
           ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 35, left: 24, right: 24, bottom: 128),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 25,
+                    width: 25,
+                    child: AssetFadeIn(
+                      urlAsset: 'assets/images/sc_1/IconFinandina.svg',
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/sc_3/ComboGroup.png',
+                  ),
+                  Column(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          style: DefaultTextStyle.of(context).style,
+                          children: const <TextSpan>[
+                            TextSpan(
+                              text: 'Paga libremente', // Resto del texto
+                              style: TextStyle(
+                                fontSize: 28.0, // Tamaño del texto
+                                fontWeight:
+                                    FontWeight.bold, // Fuente en regular
+                                color: Colors.white, // Color del texto
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          RichText(
+                            text: TextSpan(
+                              style: DefaultTextStyle.of(context).style,
+                              children: const <TextSpan>[
+                                TextSpan(
+                                  text:
+                                      'Puedes pagar tus productos, recibos y facturas',
+                                  style: TextStyle(
+                                    fontSize: 16.0, // Tamaño del texto
+                                    fontWeight:
+                                        FontWeight.normal, // Fuente en negrita
+                                    color: Colors.white, // Color del texto
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              style: DefaultTextStyle.of(context).style,
+                              children: const <TextSpan>[
+                                TextSpan(
+                                  text: 'en cualquier momento y lugar.',
+                                  style: TextStyle(
+                                    fontSize: 16.0, // Tamaño del texto
+                                    fontWeight:
+                                        FontWeight.normal, // Fuente en negrita
+                                    color: Colors.white, // Color del texto
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
