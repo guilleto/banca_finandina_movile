cab3e92e44f9110cd2af199b64856c22d6542960
Guillermo C.C <guilleccubillos@hotmail.com>
2023-09-27 22:37:59 -0500

    include in theme the family font

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.white,
-      ),
+          useMaterial3: true,
+          colorSchemeSeed: Colors.white,
+          fontFamily: "Roboto"),
       title: 'FinandinaApp',
       home: ScreenSliderPresentation(),
     );
