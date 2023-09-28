46fd1bcc8965e24d4d0fc864e3f798aafaf3ce1b
Guillermo C.C <guilleccubillos@hotmail.com>
2023-09-27 23:36:20 -0500

    Finish screen1

--- lib/src/screens/initial_1/indesx.dart
+++ lib/src/screens/initial_1/indesx.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../widgets/assets_fadeIn/index.dart';
+
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
   @override
@@ -21,6 +23,81 @@ class _InitialScreenState extends State<InitialScreen> {
             height: MediaQuery.of(context).size.height,
             fit: BoxFit.cover,
           ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 35, left: 24, right: 24, bottom: 128),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 165,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        AssetFadeIn(
+                          urlAsset: 'assets/images/sc_1/IconFinandina.svg',
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RichText(
+                              text: TextSpan(
+                                style: DefaultTextStyle.of(context).style,
+                                children: const <TextSpan>[
+                                  TextSpan(
+                                    text: 'Con ',
+                                    style: TextStyle(
+                                      fontSize: 24.0, // Tamaño del texto
+                                      fontWeight: FontWeight
+                                          .normal, // Fuente en negrita
+                                      color: Colors.white, // Color del texto
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: 'Banco Finandina', // Parte en negrita
+                                    style: TextStyle(
+                                      fontSize: 24.0, // Tamaño del texto
+                                      fontWeight:
+                                          FontWeight.bold, // Fuente en negrita
+                                      color: Colors.white, // Color del texto
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                style: DefaultTextStyle.of(context).style,
+                                children: const <TextSpan>[
+                                  TextSpan(
+                                    text:
+                                        ' tienes el poder de ser libre', // Resto del texto
+                                    style: TextStyle(
+                                      fontSize: 24.0, // Tamaño del texto
+                                      fontWeight: FontWeight
+                                          .normal, // Fuente en regular
+                                      color: Colors.white, // Color del texto
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  AssetFadeIn(
+                    urlAsset: 'assets/images/sc_1/Text2.svg',
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
